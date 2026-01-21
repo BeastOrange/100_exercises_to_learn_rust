697b6acab70bbedb7ac15ea9afeb05ea1c12f6cc
Orange <jiangjuncheng253@icloud.com>
2026-01-21 16:43:46 +0800

    Part2 Completed (34/193)

--- ABasicCalculator/LoopsFor/Task/src/lib.rs
+++ ABasicCalculator/LoopsFor/Task/src/lib.rs
@@ -1,4 +1,9 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    /* TODO */
+    let mut result: u32 = 1;
+    let i = n;
+    for i in 1..=i {
+        result *= i;
+    }
+    result
 }
